17d66431af8f0e4b2f59526d52d8162a9129565a
Lincoln Anderson <landerson@threefivetwo.com>
2022-11-25 16:21:58 -0500

    add Examples Page

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import ExamplesPage from './pages/ExamplesPage';
 import { HomePage } from './pages/HomePage';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/examples',
+    element: <ExamplesPage />,
   },
 ]);
 
