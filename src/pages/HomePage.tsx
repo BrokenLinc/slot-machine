18fced4300f08081b5add058a0bbd90a13a30979
Lincoln Anderson <landerson@threefivetwo.com>
2022-11-25 16:21:47 -0500

    remove icon macro method

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid as icons } from '@fortawesome/fontawesome-svg-core/import.macro';
+import * as icons from '@fortawesome/pro-solid-svg-icons';
 
 export const HomePage: React.FC = () => {
   return (
     <div>
       <h1>Home Page</h1>
-      <FontAwesomeIcon icon={icons('save')} />
+      <FontAwesomeIcon icon={icons.faCherries} />
     </div>
   );
 };
