e2a55551047f7fa825b4b59f2c856bda9809752d
Lincoln Anderson <landerson@threefivetwo.com>
2022-11-25 15:47:34 -0500

    install FontAwesome (solid pro)

--- craco.config.js
+++ craco.config.js
@@ -1,1 +1,3 @@
-module.exports = {};
+module.exports = {
+  babel: { plugins: ['macros'] },
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid as icons } from '@fortawesome/fontawesome-svg-core/import.macro';
+
 export const HomePage: React.FC = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <FontAwesomeIcon icon={icons('save')} />
     </div>
   );
 };
